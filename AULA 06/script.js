b422c0ff5144676dfe362027bf2b30a96840a50c
Caio S. Duarte <165346253+vindexinsanctirex@users.noreply.github.com>
2025-08-29 18:59:57 +0000

    att

---
+++ AULA 06/script.js
@@ -0,0 +1,11 @@
+function exibirMensagem () {
+    console.log("Bem vindo ao JavaScript!")
+}
+
+exibirMensagem();
+
+function saudacao(nome) {
+    console.log(`Bem-vindo, ${nome}`)
+}
+
+saudacao("Caio");
